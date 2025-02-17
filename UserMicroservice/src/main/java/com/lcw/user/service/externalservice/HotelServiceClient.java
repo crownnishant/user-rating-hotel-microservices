e96c4e51fc0ca626a1c1118a06a2c8a2f1d2e720
kumar nishant <kumarnishu82@gmail.com>
2025-02-17 22:10:24 +0530

    added comments

--- UserMicroservice/src/main/java/com/lcw/user/service/externalservice/HotelServiceClient.java
+++ UserMicroservice/src/main/java/com/lcw/user/service/externalservice/HotelServiceClient.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//implementing feign client
 @FeignClient(name="HOTELSERVICE")
 public interface HotelServiceClient {
 
